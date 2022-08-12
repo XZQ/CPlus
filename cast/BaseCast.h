d29fd5810a01e8e3f921d14cd726525bc4cea676
夏志强 <xiazhiqiang1@xiaomi.com>
2022-08-12 17:05:30 +0800

    feat : c plus study

---
+++ cast/BaseCast.h
@@ -0,0 +1,20 @@
+//
+// Created by Xia on 2022/8/3.
+//
+
+#ifndef CPLUS_BASECAST_H
+#define CPLUS_BASECAST_H
+
+
+class BaseCast {
+
+public:
+
+    virtual ~BaseCast() {
+
+    }
+
+};
+
+
+#endif //CPLUS_BASECAST_H
---
+++ templates/Stack.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Xia on 2022/8/1.
+//
+
+#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+
+void testClass() {
+    Stack<int> st1;      // 指定存储int
+    Stack<double> st2;   // 指定存储double
+}
+
+template<class T>
+void Stack<T>::Push(const T &x) {
+
+}
+
